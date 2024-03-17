8a74c8765519923c5d3df6ee8ae682cfb9186a75
NaoufalBK <nawfalbenkmil2@gmail.com>
2024-03-17 15:17:32 +0100

    Define generate method
    Co-authored-by : Mehdialaoui02 mehdi2002alaoui@gmail.com

--- expr/basic/toPfx.ml
+++ expr/basic/toPfx.ml
@@ -1,7 +1,24 @@
 open Ast
 
-let generate = function
-  | Const _ -> failwith "To implement"
-  | Binop(_,_,_) -> failwith "To implement"
-  | Uminus _ -> failwith "To implement"
+let rec generate_expr = function
+  | Const n -> [Push n]
+  | Binop (op, e1, e2) ->
+    let cmds_e1 = generate_expr e1 in
+    let cmds_e2 = generate_expr e2 in
+    cmds_e1 @ cmds_e2 @
+    (match op with
+    | BinOp.Badd -> [Add]
+    | BinOp.Bsub -> [Sub]
+    | BinOp.Bmul -> [Mul]
+    | BinOp.Bdiv -> [Div]
+    | BinOp.Bmod -> [Rem])
+  | Uminus e ->
+    let cmds_e = generate_expr e in
+    cmds_e @ [Push 0; Swap; Sub]
   | Var _ -> failwith "Not yet supported"
+
+let generate = function
+  | Const _ as e -> (0, generate_expr e)
+  | Binop(_,_,_) as e -> (0, generate_expr e)
+  | Uminus _ as e -> (0, generate_expr e)
+  | Var _ as e -> failwith "Not yet supported"
